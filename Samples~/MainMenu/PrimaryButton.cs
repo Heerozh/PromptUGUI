a21d6b25e57cac71a135c626b63e4804e2486f33
heerozh <heeroz@gmail.com>
2026-05-08 00:01:27 +0800

    feat(samples): PrimaryButton/DangerButton sample custom controls

---
+++ Samples~/MainMenu/PrimaryButton.cs
@@ -0,0 +1,24 @@
+using PromptUGUI.Controls;
+using PromptUGUI.Registry;
+using R3;
+using TMPro;
+using UnityEngine.UI;
+
+namespace PromptUGUI.Samples.MainMenu {
+    public sealed class PrimaryButton : Control {
+        Button _btn;
+        TMP_Text _label;
+
+        public override void OnAttached() {
+            _btn = GameObject.GetComponent<Button>();
+            _label = GameObject.GetComponentInChildren<TMP_Text>();
+        }
+
+        [UIAttr("text")]
+        public string TextValue {
+            set => _label.text = value;
+        }
+
+        public Observable<Unit> OnClick => _btn.OnClickAsObservable();
+    }
+}
