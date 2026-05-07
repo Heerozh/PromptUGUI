072ae01f785af7093e142c87e9d55c191aac4ca1
heerozh <heeroz@gmail.com>
2026-05-07 23:43:22 +0800

    feat(registry): ControlMeta with cached setters for [UIAttr] properties

---
+++ Runtime/Registry/ControlMeta.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+
+namespace PromptUGUI.Registry {
+    public sealed class ControlMeta {
+        readonly Dictionary<string, Action<object, string>> _setters;
+
+        ControlMeta(Dictionary<string, Action<object, string>> setters) {
+            _setters = setters;
+        }
+
+        public bool HasAttribute(string name) => _setters.ContainsKey(name);
+
+        public void Apply(object instance, string name, string value) {
+            if (!_setters.TryGetValue(name, out var setter))
+                throw new ArgumentException($"unknown attribute '{name}'");
+            setter(instance, value);
+        }
+
+        public static ControlMeta Build(Type type) {
+            var setters = new Dictionary<string, Action<object, string>>();
+
+            foreach (var prop in type.GetProperties(
+                BindingFlags.Public | BindingFlags.Instance)) {
+                var attr = prop.GetCustomAttribute<UIAttrAttribute>();
+                if (attr == null) continue;
+                if (!prop.CanWrite) continue;
+
+                var name = attr.Name ?? CamelCase(prop.Name);
+                var setter = BuildSetter(prop);
+                setters[name] = setter;
+            }
+
+            return new ControlMeta(setters);
+        }
+
+        static string CamelCase(string s) =>
+            string.IsNullOrEmpty(s) ? s : char.ToLowerInvariant(s[0]) + s.Substring(1);
+
+        static Action<object, string> BuildSetter(PropertyInfo prop) {
+            var t = prop.PropertyType;
+            if (t == typeof(string)) {
+                return (obj, v) => prop.SetValue(obj, v);
+            }
+            if (t == typeof(int)) {
+                return (obj, v) => prop.SetValue(obj,
+                    int.Parse(v, CultureInfo.InvariantCulture));
+            }
+            if (t == typeof(float)) {
+                return (obj, v) => prop.SetValue(obj,
+                    float.Parse(v, CultureInfo.InvariantCulture));
+            }
+            if (t == typeof(bool)) {
+                return (obj, v) => prop.SetValue(obj, bool.Parse(v));
+            }
+            throw new NotSupportedException(
+                $"[UIAttr] on {prop.DeclaringType.Name}.{prop.Name}: " +
+                $"type {t} not supported in M1");
+        }
+    }
+}
---
+++ Tests/EditMode/Registry/ControlMetaTests.cs
@@ -0,0 +1,51 @@
+using NUnit.Framework;
+using PromptUGUI.Registry;
+
+namespace PromptUGUI.Tests.Registry {
+    public class ControlMetaTests {
+
+        class Sample {
+            [UIAttr] public string Text { get; set; }
+            [UIAttr("count")] public int Count { get; set; }
+            public string IgnoredProp { get; set; }
+        }
+
+        [Test]
+        public void Builds_setter_for_each_UIAttr_property() {
+            var meta = ControlMeta.Build(typeof(Sample));
+            Assert.IsTrue(meta.HasAttribute("text"));
+            Assert.IsTrue(meta.HasAttribute("count"));
+            Assert.IsFalse(meta.HasAttribute("ignoredProp"));
+        }
+
+        [Test]
+        public void String_setter_assigns_value_directly() {
+            var meta = ControlMeta.Build(typeof(Sample));
+            var s = new Sample();
+            meta.Apply(s, "text", "Hello");
+            Assert.AreEqual("Hello", s.Text);
+        }
+
+        [Test]
+        public void Int_setter_parses_string_to_int() {
+            var meta = ControlMeta.Build(typeof(Sample));
+            var s = new Sample();
+            meta.Apply(s, "count", "42");
+            Assert.AreEqual(42, s.Count);
+        }
+
+        [Test]
+        public void Default_attribute_name_is_camelCase_of_property() {
+            var meta = ControlMeta.Build(typeof(Sample));
+            Assert.IsTrue(meta.HasAttribute("text"));
+        }
+
+        [Test]
+        public void Apply_unknown_attribute_throws() {
+            var meta = ControlMeta.Build(typeof(Sample));
+            var s = new Sample();
+            Assert.Throws<System.ArgumentException>(() =>
+                meta.Apply(s, "unknown", "x"));
+        }
+    }
+}
