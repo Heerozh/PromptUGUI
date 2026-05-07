be90113d03c67b2f2135861021763127449e6f4d
heerozh <heeroz@gmail.com>
2026-05-07 23:50:09 +0800

    feat(controls): Text primitive backed by TMP_Text

---
+++ Runtime/Controls/Text.cs
@@ -0,0 +1,48 @@
+using PromptUGUI.Registry;
+using TMPro;
+using UnityEngine;
+
+namespace PromptUGUI.Controls {
+    public sealed class Text : Control {
+        TMP_Text _tmp;
+
+        public override void OnAttached() {
+            _tmp = GameObject.GetComponent<TMP_Text>()
+                   ?? GameObject.AddComponent<TextMeshProUGUI>();
+        }
+
+        [UIAttr("text")]
+        public string TextValue {
+            set => _tmp.text = value ?? "";
+        }
+
+        [UIAttr]
+        public int Size {
+            set => _tmp.fontSize = value;
+        }
+
+        [UIAttr]
+        public string Color {
+            set {
+                if (ColorUtility.TryParseHtmlString(value, out var c))
+                    _tmp.color = c;
+            }
+        }
+
+        [UIAttr]
+        public string Align {
+            set {
+                _tmp.alignment = value switch {
+                    "center" => TextAlignmentOptions.Center,
+                    "right"  => TextAlignmentOptions.Right,
+                    _        => TextAlignmentOptions.Left,
+                };
+            }
+        }
+
+        [UIAttr]
+        public bool Wrap {
+            set => _tmp.enableWordWrapping = value;
+        }
+    }
+}
---
+++ Tests/PlayMode/Controls/TextTests.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using NUnit.Framework;
+using TMPro;
+using PromptUGUI.Controls;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace PromptUGUI.Tests.Controls {
+    public class TextTests {
+        [UnityTest]
+        public IEnumerator Adds_TMP_Text_component_on_attach() {
+            var t = new Text();
+            var go = new GameObject("text", typeof(RectTransform));
+            t.AttachTo(go);
+            Assert.IsNotNull(go.GetComponent<TMP_Text>());
+            Object.Destroy(go);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator Text_property_writes_to_TMP() {
+            var t = new Text();
+            var go = new GameObject("text", typeof(RectTransform));
+            t.AttachTo(go);
+            t.TextValue = "你好";
+            Assert.AreEqual("你好", go.GetComponent<TMP_Text>().text);
+            Object.Destroy(go);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator Size_property_writes_to_fontSize() {
+            var t = new Text();
+            var go = new GameObject("text", typeof(RectTransform));
+            t.AttachTo(go);
+            t.Size = 32;
+            Assert.AreEqual(32f, go.GetComponent<TMP_Text>().fontSize);
+            Object.Destroy(go);
+            yield return null;
+        }
+    }
+}
