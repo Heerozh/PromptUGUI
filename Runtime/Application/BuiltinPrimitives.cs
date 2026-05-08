5b551181446a554c56b8718bbb657868b982074e
heerozh <heeroz@gmail.com>
2026-05-08 13:40:53 +0000

    feat(runtime): register Icon as builtin primitive

--- Runtime/Application/BuiltinPrimitives.cs
+++ Runtime/Application/BuiltinPrimitives.cs
@@ -6,6 +6,7 @@ namespace PromptUGUI.Application {
         public static void Register(ControlRegistry reg) {
             reg.Register<Frame>("Frame", null);
             reg.Register<Image>("Image", null);
+            reg.Register<Icon>("Icon", null);
             reg.Register<Text>("Text", null, defaultTextAttr: "text");
             reg.Register<VStack>("VStack", null);
             reg.Register<HStack>("HStack", null);
