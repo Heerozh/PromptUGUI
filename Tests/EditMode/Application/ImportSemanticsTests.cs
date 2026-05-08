6915912947e6edab5a5965dbe0cf5f1c08a0e687
heerozh <heeroz@gmail.com>
2026-05-08 10:41:23 +0000

    test(import): namespaced Import disambiguates same-named templates

--- Tests/EditMode/Application/ImportSemanticsTests.cs
+++ Tests/EditMode/Application/ImportSemanticsTests.cs
@@ -32,5 +32,30 @@ namespace PromptUGUI.Tests.Application {
             var screen = UI.Open("Main");
             Assert.IsNotNull(screen.Get<Frame>("c"));
         }
+
+        [Test]
+        public void Imports_with_namespace_can_coexist_with_same_template_name() {
+            var files = new Dictionary<string, string> {
+                ["a"] = @"<?xml version='1.0'?><PromptUGUI version='1'>
+                            <Template name='Btn'><Frame/></Template>
+                          </PromptUGUI>",
+                ["b"] = @"<?xml version='1.0'?><PromptUGUI version='1'>
+                            <Template name='Btn'><Frame/></Template>
+                          </PromptUGUI>",
+                ["main"] = @"<?xml version='1.0'?><PromptUGUI version='1'>
+                               <Import src='a'/>
+                               <Import src='b' as='b'/>
+                               <Screen name='M'>
+                                 <Btn id='one'/>
+                                 <b.Btn id='two'/>
+                               </Screen>
+                             </PromptUGUI>",
+            };
+            UI.SourceResolver = src => files.TryGetValue(src, out var v) ? v : null;
+            UI.LoadDocumentFromSrc("main");
+            var s = UI.Open("M");
+            Assert.IsNotNull(s.Get<Frame>("one"));
+            Assert.IsNotNull(s.Get<Frame>("two"));
+        }
     }
 }
