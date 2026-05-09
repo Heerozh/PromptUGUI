373b1d3a518d26baaf8682bc12754edd382012c6
heerozh <heeroz@gmail.com>
2026-05-09 21:20:41 +0800

    chore: 还是去掉noNamespaceSchemaLocation，保持简洁

--- Editor/CreateUiXmlMenu.cs
+++ Editor/CreateUiXmlMenu.cs
@@ -1,46 +1,14 @@
-using System;
-using System.IO;
 using UnityEditor;
-using UnityEngine;
 
 namespace PromptUGUI.Editor
 {
     internal static class CreateUiXmlMenu
     {
         private const string DefaultFilename = "NewUI.ui.xml";
-        private const string XsdAssetPath = "Assets/PromptUGUI.gen.xsd";
 
-        [MenuItem("Assets/Create/PromptUGUI/UI XML", false, 81)]
-        private static void CreateUiXml()
-        {
-            var folder = ResolveSelectedFolder();
-            var schemaRel = ComputeRelativePath(folder, XsdAssetPath);
-            var content = BuildTemplate(schemaRel);
-            ProjectWindowUtil.CreateAssetWithTextContent(DefaultFilename, content);
-        }
-
-        private static string ResolveSelectedFolder()
-        {
-            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
-            if (string.IsNullOrEmpty(path)) return "Assets";
-            return AssetDatabase.IsValidFolder(path)
-                ? path
-                : Path.GetDirectoryName(path).Replace('\\', '/');
-        }
-
-        private static string ComputeRelativePath(string fromDir, string toFile)
-        {
-            var from = new Uri(Path.GetFullPath(fromDir) + Path.DirectorySeparatorChar);
-            var to = new Uri(Path.GetFullPath(toFile));
-            return Uri.UnescapeDataString(from.MakeRelativeUri(to).ToString())
-                     .Replace('\\', '/');
-        }
-
-        private static string BuildTemplate(string schemaRel) =>
-$@"<?xml version=""1.0"" encoding=""utf-8""?>
-<PromptUGUI version=""1""
-            xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""
-            xsi:noNamespaceSchemaLocation=""{schemaRel}"">
+        private const string Template =
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<PromptUGUI version=""1"">
 
   <Screen name=""NewScreen"">
     <Frame anchor=""stretch""/>
@@ -48,5 +16,11 @@ $@"<?xml version=""1.0"" encoding=""utf-8""?>
 
 </PromptUGUI>
 ";
+
+        [MenuItem("Assets/Create/PromptUGUI/UI XML", false, 81)]
+        private static void CreateUiXml()
+        {
+            ProjectWindowUtil.CreateAssetWithTextContent(DefaultFilename, Template);
+        }
     }
 }
