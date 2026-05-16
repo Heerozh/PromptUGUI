54745d3fd176ded41f46a275041e1037718b3bf8
heerozh <heeroz@gmail.com>
2026-05-16 19:49:41 +0800

    chore: fix unity 6 api

--- Editor/CreateUiXmlMenu.cs
+++ Editor/CreateUiXmlMenu.cs
@@ -1,4 +1,7 @@
+using System.IO;
 using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
 
 namespace PromptUGUI.Editor
 {
@@ -24,7 +27,29 @@ namespace PromptUGUI.Editor
         [MenuItem("Assets/Create/PromptUGUI/UI XML", false, 81)]
         private static void CreateUiXml()
         {
-            ProjectWindowUtil.CreateAssetWithTextContent(DefaultFilename, Template);
+#if UNITY_6000_6_OR_NEWER
+              ProjectWindowUtil.CreateAssetWithTextContent(DefaultFilename, Template);
+#else
+            var icon = EditorGUIUtility.IconContent("TextAsset Icon").image as Texture2D;
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(
+                0,
+                ScriptableObject.CreateInstance<DoCreateUiXml>(),
+                DefaultFilename,
+                icon,
+                null);
+#endif
         }
+
+#if !UNITY_6000_6_OR_NEWER
+        private sealed class DoCreateUiXml : EndNameEditAction
+        {
+            public override void Action(int instanceId, string pathName, string resourceFile)
+            {
+                File.WriteAllText(pathName, Template);
+                AssetDatabase.ImportAsset(pathName);
+                ProjectWindowUtil.ShowCreatedAsset(AssetDatabase.LoadAssetAtPath<Object>(pathName));
+            }
+        }
+#endif
     }
 }
