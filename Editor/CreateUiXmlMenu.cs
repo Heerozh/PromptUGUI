e6d5545608e0f4a660d43f0f9a97ca3aee55024c
heerozh <heeroz@gmail.com>
2026-05-28 15:55:25 +0800

    feat: add create template menu

--- Editor/CreateUiXmlMenu.cs
+++ Editor/CreateUiXmlMenu.cs
@@ -7,9 +7,10 @@ namespace PromptUGUI.Editor
 {
     internal static class CreateUiXmlMenu
     {
-        private const string DefaultFilename = "NewUI.ui.xml";
+        private const string DefaultScreenFilename = "NewUI.ui.xml";
+        private const string DefaultTemplateFilename = "NewTemplate.ui.xml";
 
-        private const string Template =
+        private const string ScreenContent =
 @"<?xml version=""1.0"" encoding=""utf-8""?>
 <PromptUGUI version=""1"">
 
@@ -24,17 +25,38 @@ namespace PromptUGUI.Editor
 </PromptUGUI>
 ";
 
+        private const string TemplateContent =
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<PromptUGUI version=""1"">
+
+  <Template name=""NewTemplate"">
+    <Param name=""label"" default=""""/>
+    <Frame>
+      <Text anchor=""stretch"" align=""center"">Label: {{label}}</Text>
+    </Frame>
+  </Template>
+
+</PromptUGUI>
+";
+
         [MenuItem("Assets/Create/PromptUGUI/UI XML", false, 81)]
-        private static void CreateUiXml()
+        private static void CreateUiXml() => Create(DefaultScreenFilename, ScreenContent);
+
+        [MenuItem("Assets/Create/PromptUGUI/UI Template", false, 82)]
+        private static void CreateUiTemplate() => Create(DefaultTemplateFilename, TemplateContent);
+
+        private static void Create(string filename, string content)
         {
 #if UNITY_6000_6_OR_NEWER
-              ProjectWindowUtil.CreateAssetWithTextContent(DefaultFilename, Template);
+            ProjectWindowUtil.CreateAssetWithTextContent(filename, content);
 #else
             var icon = EditorGUIUtility.IconContent("TextAsset Icon").image as Texture2D;
+            var action = ScriptableObject.CreateInstance<DoCreateUiXml>();
+            action.Content = content;
             ProjectWindowUtil.StartNameEditingIfProjectWindowExists(
                 0,
-                ScriptableObject.CreateInstance<DoCreateUiXml>(),
-                DefaultFilename,
+                action,
+                filename,
                 icon,
                 null);
 #endif
@@ -43,9 +65,10 @@ namespace PromptUGUI.Editor
 #if !UNITY_6000_6_OR_NEWER
         private sealed class DoCreateUiXml : EndNameEditAction
         {
+            public string Content;
             public override void Action(int instanceId, string pathName, string resourceFile)
             {
-                File.WriteAllText(pathName, Template);
+                File.WriteAllText(pathName, Content);
                 AssetDatabase.ImportAsset(pathName);
                 ProjectWindowUtil.ShowCreatedAsset(AssetDatabase.LoadAssetAtPath<Object>(pathName));
             }
