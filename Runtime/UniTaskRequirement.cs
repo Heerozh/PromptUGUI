9a747dfa8f2b6b2fb41d7d19a885127973056257
heerozh <heeroz@gmail.com>
2026-07-06 15:14:38 +0800

    chore: unitask url

--- Runtime/UniTaskRequirement.cs
+++ Runtime/UniTaskRequirement.cs
@@ -1,5 +1,5 @@
 // PromptUGUI on Unity 2022 requires UniTask. This guard turns hundreds of
 // "Awaitable not found" errors into one clear message when UniTask is missing.
 #if !UNITY_6000_0_OR_NEWER && !PROMPTUGUI_HAS_UNITASK
-#error PromptUGUI on Unity 2022 requires UniTask (com.cysharp.unitask). Install it via OpenUPM: https://openupm.com/packages/com.cysharp.unitask/
+#error PromptUGUI on Unity 2022 requires UniTask (com.cysharp.unitask). Install it via : https://github.com/Cysharp/UniTask.git?path=src/UniTask/Assets/Plugins/UniTask
 #endif
